b42654ac6aa6ef616d5707fd6b31d957dad95c72
Vandana S <98863444+vandana-aa@users.noreply.github.com>
2024-06-11 20:20:51 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,9 +15,9 @@ if fl is not None:
     filename = fl.name
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
-else:
-    os.chdir("/Users/vandana/Desktop/Streamlit")
-    df = pd.read_csv("SampleSuperstore.csv", encoding = "ISO-8859-1")
+#else:
+#    os.chdir("/Users/vandana/Desktop/Streamlit")
+ #   df = pd.read_csv("SampleSuperstore.csv", encoding = "ISO-8859-1")
 
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
